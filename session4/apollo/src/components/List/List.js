870d06b2513ce89451037bedbe47ec9eec7e4c62
Elbert Castañeda <elbert@sbxretail.com>
2018-03-20 20:20:28 -0600

    My demo-1

--- session4/apollo/src/components/List/List.js
+++ session4/apollo/src/components/List/List.js
@@ -1,11 +1,43 @@
 import React, { Component } from "react";
 import Pokemon from "../Pokemon";
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 import { Container } from "./List.styled";
 
 class PokemonList extends Component {
   render() {
-    return <Container />;
+    console.log(this.props);
+    const {pokemonQuery, onSelect } = this.props;
+
+    if (!pokemonQuery) {
+      return null;
+    }
+    
+    if (pokemonQuery.loading) {
+      return <div>Loading ...</div>;
+    }
+
+    const { pokemons } = pokemonQuery;
+    return <Container>
+      { pokemons.map(pokemon => 
+          <Pokemon key={pokemon.id} 
+            id={pokemon.id} 
+            image={pokemon.image} 
+            number={pokemon.number}
+            name={pokemon.name} />)}
+    </Container>;
   }
 }
 
-export default PokemonList;
+const POKEMON_QUERY = gql`
+  query GetPokemon {
+    pokemons(first: 151) {
+      id
+      name
+      number
+      image
+    }
+  }
+`;
+
+export default graphql(POKEMON_QUERY, { name: 'pokemonQuery'})(PokemonList);
